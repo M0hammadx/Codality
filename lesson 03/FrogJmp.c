73d63738bf6fdea24ee53ab7cb238ee843606c9b
Mohammad Abou El Sherbini <mh-sherbini@hotmail.com>
2018-07-05 13:03:14 +0200

    Create  FrogJmp.c

---
+++ lesson 03/FrogJmp.c
@@ -0,0 +1,8 @@
+// you can write to stdout for debugging purposes, e.g.
+// printf("this is a debug message\n");
+
+int solution(int x, int y, int d) {
+    // write your code in C99 (gcc 6.2.0)
+    
+    return ceil((y-x)*1./d);
+}
