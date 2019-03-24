8d387f63b398cb33904f15b8c4743d00d4ee4194
Mohammad Khaled Abou El Sherbini <mh-sherbini@hotmail.com>
2019-03-24 18:39:28 +0200

    Create Fish.cpp

---
+++ Lesson 07/Fish.cpp
@@ -0,0 +1,21 @@
+// you can use includes, for example:
+	// #include <algorithm>
+#include <bits/stdc++.h>
+	// you can write to stdout for debugging purposes, e.g.
+	// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A, vector<int> &B) {
+	// write your code in C++14 (g++ 6.2.0)
+	stack<int> s0,s1;
+	int dir = B[0];
+	for (int i = 0; i < A.size(); i++) {
+		if (B[i] == 1) {
+			s1.push(A[i]);
+		} else {
+			s0.push(A[i]);
+			while(s1.size()>0&&A[i]>s1.top())s1.pop();
+			if(s1.size())s0.pop();
+		}
+	}
+	return s0.size()+s1.size();
+}
